0f1bbd3d6d2521d93790fa4a3160825c0105387c
TYLERSANDOVAL\Student <tyler.sandoval@outlook.com>
2018-08-13 14:35:37 -0500

    Initial Commit: Built 2-tier application with Identity and edmx data model

---
+++ FSDP.DATA.EF/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSDP.DATA.EF
+{
+    public class Class1
+    {
+    }
+}
