5058d7d40d094ab7a210e5d8c646b4f716643703
TYLERSANDOVAL\Student <tyler.sandoval@outlook.com>
2018-08-13 22:54:03 -0500

    COMMIT: Added Utilties Folder and FileUtilities.cs;

---
+++ FSDP.UI.MVC/Utilties/FileUtilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace FSDP.UI.MVC.Utilties
+{
+    public class FileUtilities
+    {
+        public static void UploadFile(string savePath, string fileName, HttpPostedFileBase file)
+        {
+            file.SaveAs(savePath + fileName);
+        }
+
+        public static void DeleteFile(string path)
+        {
+            // get info about the specified file
+            FileInfo file = new FileInfo(path);
+
+            // check if the file exists
+            if (file.Exists)
+            {
+                // If so delete the file
+                file.Delete();
+            }
+        }
+    }
+}
